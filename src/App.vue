6b5eca3ee8684351de60b052bced90743a95f08e
reesewesterhoff <39221548+reesewesterhoff@users.noreply.github.com>
2018-12-06 09:54:48 -0600

    finish nesting components

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<!-- display components in the order you want them -->
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+// import components to show them
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+  // tell where to find component and what we want to call it
+  components: {
+    'app-header': Header,
+    'app-ninjas': Ninjas,
+    'app-footer': Footer
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+      <p>{{copyright}}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        copyright: 'Copyright 2018 Vue Ninjas'
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer {
+    background-color: #222;
+    padding: 6px;
+}
+
+p {
+    color: lightgreen;
+    text-align: center;
+}
+</style>
